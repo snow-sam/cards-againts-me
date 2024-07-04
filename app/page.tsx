7626b1ce89f9c9bbfdf36629834ff992b1dc0b81
Samuel Souza <samuel_f2@icloud.com>
2024-07-04 17:47:14 -0300

    ref: passa a usar dominio fornecido pela vercel para usar os dados fakes iniciais.

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import { Options } from "@/components/Options";
 
 
 export default async function Home() {
-  //const cardsTexts = await fetch(`${process.env.BASE_URL}/arq.json`).then(req=>req.json())
-  const cardsTexts = ['']
+  const cardsTexts = await fetch(`${process.env.BASE_URL}/arq.json`).then(req=>req.json())
   console.log(cardsTexts)
   return (
     <main className="w-full h-[100dvh] grid place-items-center bg-neutral-200 relative overflow-hidden">
