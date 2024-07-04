31e019c6981bd4870fb33291b61939845643c90e
Samuel Souza <samuel_f2@icloud.com>
2024-07-04 17:41:03 -0300

    fix: url base para fetch de dados fake iniciais

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Options } from "@/components/Options";
 
 
 export default async function Home() {
-  const cardsTexts = await fetch('http://192.168.15.19:3000/arq.json').then(req=>req.json())
+  const cardsTexts = await fetch(`${process.env.BASE_URL}/arq.json`).then(req=>req.json())
   console.log(cardsTexts)
   return (
     <main className="w-full h-[100dvh] grid place-items-center bg-neutral-200 relative overflow-hidden">
